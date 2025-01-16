aac405136e0fd9b7021bdb422d115635f39e4f06
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-01-17 10:58:53 +1100

    Add header

---
+++ app/components/header/header.tsx
@@ -0,0 +1,9 @@
+﻿import './header.scss';
+
+export default function Header() {
+    return (
+        <header className={"header"}>
+            <h1>insert name here</h1>
+        </header>
+    );
+}

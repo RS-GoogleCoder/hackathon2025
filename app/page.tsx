ff32ade4c89e493581cf02a23279d64b3e1e2724
cozmo <sheldonmachado11@gmail.com>
2025-01-17 12:02:53 +1100

    css glowing stuff

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,17 @@ export default function Page() {
     return (
         <>
             <div className={"heading-container"}><h1 className={"top-picks"}>Top Picks</h1></div>
+            <div className="rec-container">
+              <div className="rec-1">
 
+              </div>
+              <div className="rec-2">
+
+              </div>
+              <div className="rec-3">
+                
+              </div>
+            </div>
         </>
     )
 }

b2622bd53d500a6afe165b870d7d29d4dbecb07c
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-01-17 13:58:35 +1100

    Carrier structure

---
+++ app/data/carrier.ts
@@ -0,0 +1,10 @@
+﻿export class Carrier {
+    name: string;
+    rating: number;
+
+    constructor(name: string, rating: number) {
+        this.name = name;
+        if (rating < 0 || rating > 5) throw new Error("Rating must be between 0 and 5")
+        this.rating = rating;
+    }
+}

cd65daea3597b4988f5972bc74a87d5cede4b88b
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-01-18 11:10:22 +1100

    Topbar fix

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,9 +1,11 @@
 ﻿import './header.scss';
-
+import Jawa from "@/public/jawa.png";
+import Image from "next/image";
 export default function Header() {
     return (
         <header className={"header"}>
-            <h1>TripJawa</h1>
+            <h1 style={{height: "min-content"}}><a href={"/"}>TripJawa</a></h1>
+            <Image src={Jawa} alt={"Jawa"} style={{height: "100%", width: "auto"}}/>
             <nav className={"nav-links"}>
                 <a href={"/"}>Home</a>
                 <a href={"/booking"}>Booking</a>

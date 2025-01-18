ed78a8a9f4527f8bdff1a9d080a5414eb8847771
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-01-18 13:06:57 +1100

    Header fixed

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header className={"header"}>
-            <h1 style={{height: "min-content"}}><a href={"/"}>TripJawa</a></h1>
-            <Image src={Jawa} alt={"Jawa"} style={{height: "100%", width: "auto"}}/>
+            <a style={{display: "flex", alignItems: "center"}} href={"/"}>
+                <h1>TripJawa</h1>
+                <Image src={Jawa} alt={"Jawa"} style={{height: "2rem", width: "auto", marginLeft: "0.5rem"}}/>
+            </a>
             <nav className={"nav-links"}>
                 <a href={"/"}>Home</a>
                 <a href={"/booking"}>Booking</a>

a4bc267aabcec3d44ea177009fa6e8a2a4a09830
Gergo Kaszas <gergo.kaszas@outlook.com>
2025-01-19 11:37:27 +1100

    Carousel on home page

---
+++ app/components/carousel/carousel.tsx
@@ -0,0 +1,55 @@
+﻿import React, {useState, useEffect} from 'react';
+import './carousel.scss';
+
+interface CarouselProps {
+    children: React.ReactNode[];
+    interval?: number;
+    showControls: boolean;
+    reverse: boolean;
+}
+
+const Carousel: React.FC<CarouselProps> = ({children, interval = 3000, showControls = false, reverse = false}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % children.length);
+        }, interval);
+
+        return () => clearInterval(timer);
+    }, [children.length, interval]);
+
+    const handlePrevious = () => {
+        if (!reverse) setCurrentIndex((currentIndex - 1 + children.length) % children.length);
+        else setCurrentIndex((currentIndex + 1) % children.length);
+    };
+
+    const handleNext = () => {
+        if (!reverse) setCurrentIndex((currentIndex + 1) % children.length);
+        else setCurrentIndex((currentIndex - 1 + children.length) % children.length);
+    };
+
+    const getVisibleItems = () => {
+        const items = [];
+        for (let i = 0; i < 3; i++) {
+            items.push(children[(currentIndex + i) % children.length]);
+        }
+        return items;
+    };
+
+    return (
+        <div className="carousel">
+            {getVisibleItems().map((child, index) => (
+                <div key={index} className="carousel-item active">
+                    {child}
+                </div>
+            ))}
+            {showControls && <div className="carousel-controls">
+                <button onClick={handlePrevious}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+            </div>}
+        </div>
+    );
+};
+
+export default Carousel;
--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,52 @@
 import {Attraction, Attractions} from "@/app/data/attraction";
 import {useEffect, useState} from "react";
 import Image from "next/image";
+import Carousel from "@/app/components/carousel/carousel";
 import "./attractions/attractions.scss";
+import {Carrier, Carriers} from "./data/carrier";
 
 export default function Page() {
-    const [attractions, setAttractions] = useState<Attraction[]>([]);
+    const [topRatedAttractions, setTopRatedAttractions] = useState<Attraction[]>([]);
+    const [topRatedCarriers, setTopRatedCarriers] = useState<Carrier[]>([]);
 
     useEffect(() => {
-        const sortedAttractions = Attractions.sort((a, b) => b.rating - a.rating);
-        setAttractions(sortedAttractions.slice(0, 3)); // Get top 3 attractions
+        const filteredAttractions = Attractions.filter(a => a.rating >= 4.7 && a.rating <= 4.9);
+        setTopRatedAttractions(filteredAttractions);
+
+        const filteredCarriers = Carriers.filter(a => a.rating >= 4 && a.rating <= 4.9);
+        setTopRatedCarriers(filteredCarriers);
     }, []);
 
     return <>
         <h1 style={{textAlign: "center"}}>Our top picks</h1>
-        <div className={"attractions-container"}>
-            {attractions.map(a => <div key={a.name} className={"attraction"}>
-                <h2>{a.name}</h2>
-                <Image src={"/attractions/" + a.image} alt={a.name} width={200} height={200} style={{width: "auto"}}/>
-                <p>{a.description}</p>
-                <p>Rating: {a.rating}⭐</p>
-                <p>Location: {a.locationLocal}, {a.location.name}
-                    <button onClick={() => {
-                        window.location.href = "/booking?planet=" + a.location.name;
-                    }}>Go
-                    </button>
-                </p>
-            </div>)
-            }
-        </div>
+        <Carousel interval={3000} showControls={false} reverse={false}>
+            {topRatedAttractions.map(a => (
+                <div key={a.name} className={"attraction"}>
+                    <h2>{a.name}</h2>
+                    <Image src={"/attractions/" + a.image} alt={a.name} width={200} height={200}
+                           style={{width: "auto"}}/>
+                    <p>{a.description}</p>
+                    <p>Rating: {a.rating}⭐</p>
+                    <p>Location: {a.locationLocal}, {a.location.name}
+                        <button onClick={() => {
+                            window.location.href = "/booking?planet=" + a.location.name;
+                        }}>Go
+                        </button>
+                    </p>
+                </div>
+            ))}
+        </Carousel>
+        <h1 style={{textAlign: "center"}}>Travel with:</h1>
+        <Carousel interval={3000} showControls={false} reverse={true}>
+            {topRatedCarriers.map(a => (
+                <div key={a.name} className={"attraction"}>
+                    <h2>{a.name}</h2>
+                    <Image src={"/carriers/" + a.image} alt={a.name} width={200} height={200}
+                           style={{width: "auto"}}/>
+                    <p>Rating: {a.rating}⭐</p>
+                </div>
+            ))}
+        </Carousel>
         <div style={{textAlign: "center", fontSize: "1.5em", marginTop: "4rem"}}>
             <a href={"/booking"} style={{textDecoration: "none"}}>Bookings</a> • <a href={"/attractions"}
                                                                                     style={{textDecoration: "none"}}>Attractions</a> • <a
